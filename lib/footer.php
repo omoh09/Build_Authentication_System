83d98739ec50e5925ca23978dbfd23f4c1cc2bdc
omoh09 <gabrielifoga@yahoo.com>
2020-03-31 11:20:28 +0100

    Add files via upload

--- lib/footer.php
+++ lib/footer.php
@@ -1,1 +1,20 @@
 
+<!-- MENU -->
+    <p> 
+        <a href="index.php">Home</a> |
+        <?php if(!isset($_SESSION['loggedIn'])){ ?>
+        
+        <a href="login.php">Login</a> |
+        <a href="register.php">Register</a> |
+
+        <?php }else{ ?>
+            
+        <a href="logout.php">Logout</a>
+
+        <?php } ?>
+        <a href="forgot.php">Forgot Password</a>
+    </p>
+</body>
+
+
+</html>
---
+++ lib/header.php
@@ -0,0 +1,9 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to SNG : Hospital for the ignorant</title>
+</head>
+<body>
