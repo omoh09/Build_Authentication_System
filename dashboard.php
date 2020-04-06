a1e4bce437f72b3fe2945da3e84d62059641f1b4
omoh09 <gabrielifoga@yahoo.com>
2020-04-06 04:36:09 -0700

    ajusted

--- dashboard.php
+++ dashboard.php
@@ -5,27 +5,68 @@ if(!isset($_SESSION['loggedIn'])){
     header("Location: login.php");
 }
 
+$allUserslogin = scandir("db/login/");
+$countAllUserslogin = count($allUserslogin);
+
+for ($counter = 0; $counter < $countAllUserslogin ; $counter++) {
+   
+    $currentUserlogin = $allUserslogin[$counter];
+	$currentUserlogin == $_SESSION['email'] . ".json";
+    $userlogin = file_get_contents("db/login/".$currentUserlogin);
+    $userObject = json_decode($userlogin);
+    $dblogin = $userObject->Last_LOGIN;
+	
+}
 /*$date = [
 		'last_login' => date("Y-m-d, h:i:sa")
 	];
 file_put_contents("db/users/". $_SESSION['loggedIn'] . ".json", json_encode($date),FILE_APPEND);*/
 ?>
-<h3>Dashboard</h3>
-    LoggedIn User ID: <?php echo $_SESSION['loggedIn'] ?>
-    <br />User Access Level: Menber*<?php //echo $_SESSION['first_name'] ?>
-    <br />Firstname: <?php echo $_SESSION['first_name'] ?>
-    <br />Lastname: <?php echo $_SESSION['last_name'] ?>
-    <br />Email: <?php echo $_SESSION['email'] ?>
-    <br />Department: <?php echo $_SESSION['department'] ?>
-    <br />Registration Date: <?php echo $_SESSION['reg_date'] ?>
-    <br />Last Login: <?php echo $_SESSION['reg_date'] ?>
-	<?php
+<header>
+		<div class="appname"><a href="index.php">Home</a>&nbsp; |</div> 
+		<div class="appname">&nbsp; Dashboard</div>
+		<div class="loggedin">Welcome <?php echo $_SESSION['first_name']." ".$_SESSION['last_name']; ?>! <a href="logout.php">Log Out</a></div>
+	</header>
+	
+<main>
+<table class="masterlist">
+			<tr>
+				<th>My Details</th>
+				<th></th>
+			</tr>
+			<tr>
+				<td>Full Name</td>
+				<td><?php echo $_SESSION['first_name']." ".$_SESSION['last_name']; ?></td>
+			</tr>
+			<tr>
+				<td>LoggedIn User ID</td>
+				<td><?php echo $_SESSION['loggedIn'] ?></td>
+			</tr>
+			<tr>
+				<td>User Access Level: Menber</td>
+				<td>Menber**</td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td><?php echo $_SESSION['email'] ?></td>
+			</tr>
+			<tr>
+				<td>Department</td>
+				<td><?php echo $_SESSION['department'] ?></td>
+			</tr>
+			<tr>
+				<td>Registration Date</td>
+				<td><?php echo $_SESSION['reg_date'] ?></td>
+			</tr>
+			<tr>
+				<td>Last Login</td>
+				<td><?php echo $dblogin; ?></td>
+			</tr>
+		</table>
 
-		
-		
-		
-		
-		
-	?>
+</main>
 
-<?php include_once('lib/footer.php'); ?>
+</body>
+
+
+</html>
