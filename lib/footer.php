053c498ad6c238fcfc800e29e607c5a8a427a9f4
omoh09 <gabrielifoga@yahoo.com>
2020-04-13 14:22:20 +0800

    Add files via upload

--- lib/footer.php
+++ lib/footer.php
@@ -1,19 +1,36 @@
 
 <!-- MENU -->
     <p> 
-        <a href="index.php">Home</a> |
-        <?php if(!isset($_SESSION['loggedIn'])){ ?>
         
-        <a href="login.php">Login</a> |
-        <a href="register.php">Register</a> |
-
+        <?php if(!isset($_SESSION['loggedIn'])){ ?>
+        <a href="index.php">Home</a> |&nbsp;
+        <a href="login.php">Login</a> |&nbsp;
+        <a href="register.php">Register</a> |&nbsp;
+		<a href="forgot.php">Forgot Password</a>
+	</p>
+  </div>
         <?php }else{ ?>
-            
-        <a href="logout.php">Logout</a>
-
-        <?php } ?>
-        <a href="forgot.php">Forgot Password</a>
-    </p>
+           
+        <p>
+			<a href="reset.php">Change Password</a> &nbsp;|
+		<?php
+			if($_SESSION['designation'] == 'Patient'){ ?>
+			<a href="paybill.php">Pay Bill</a> &nbsp;|
+			<a href="bookappointment.php">Book Appointment</a>
+        <?php
+		}
+			if($_SESSION['designation'] == 'Medical Team (MT)'){ ?>
+			<a href="viewappointment.php">View Appointment</a> &nbsp;|
+		<?php	
+		}
+			if(ucwords($_SESSION['designation']) == 'Medical Director (MD)'){ ?>
+			<a href="viewpatients.php">View Patients</a> &nbsp;|
+			<a href="viewstaff.php">View Staffs</a>
+		</p>
+			
+		<?php	}
+		 } ?>
+        
 </body>
 
 
