d0237a1af3905992123104dba21f34532a538e87
omoh09 <gabrielifoga@yahoo.com>
2020-04-16 03:03:57 +0800

    Delete dashboardheader.php

--- lib/dashboardheader.php
+++
@@ -1,6 +0,0 @@
-<header>
-	<div class="appname"><a href="index.php">Home</a>&nbsp; |</div> 
-	<div class="appname">&nbsp; <a href="dashboard.php">Dashboard</a></div>
-	<div class="appname">&nbsp;| <strong><?php echo ucwords($_SESSION['designation'])?></strong></div>
-	<div class="loggedin">Welcome <?php echo ucwords($_SESSION['first_name']." ".$_SESSION['last_name']); ?>! <a href="logout.php">Log Out</a></div>
-</header>
